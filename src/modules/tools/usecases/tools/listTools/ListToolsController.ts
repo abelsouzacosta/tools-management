738c3afc7b1f2eb53ae39f3f8419c087db6910a4
Abel Souza <abelsouzacosta@gmail.com>
2022-01-19 18:41:40 -0300

    feat(tools): query search
    Allows to list all tools that have an specified tag

--- src/modules/tools/usecases/tools/listTools/ListToolsController.ts
+++ src/modules/tools/usecases/tools/listTools/ListToolsController.ts
@@ -5,9 +5,15 @@ import { ListToolsUseCase } from "./ListToolsUseCase";
 
 class ListToolsController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const { tag } = request.query;
+    const parsedStringTag = tag ? tag.toString() : undefined;
     const listToolsUseCase = container.resolve(ListToolsUseCase);
 
-    const tools = await listToolsUseCase.execute();
+    const result = await listToolsUseCase.execute();
+
+    const tools = tag
+      ? result.filter((tool) => tool.tags.includes(parsedStringTag))
+      : result;
 
     return response.status(200).json(tools);
   }
